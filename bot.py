ecfa5ba21545199fe266d7e3a383b7c6af5c720d
gamer5000m-dev <gamer5000m@gmail.com>
2026-05-25 18:38:35 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -71,13 +71,23 @@ async def responder(update, context):
 
     reply_markup = InlineKeyboardMarkup(teclado)
 
-    await context.bot.send_photo(
-    chat_id=CANAL,
-    photo=link,
-    caption=mensagem,
-    parse_mode="HTML",
-    reply_markup=reply_markup
-)
+    try:
+        await context.bot.send_photo(
+            chat_id=CANAL,
+            photo=link,
+            caption=mensagem,
+            parse_mode="HTML",
+            reply_markup=reply_markup
+        )
+
+    except:
+        await context.bot.send_message(
+            chat_id=CANAL,
+            text=mensagem,
+            parse_mode="HTML",
+            disable_web_page_preview=False,
+            reply_markup=reply_markup
+        )
 
 
 app = Application.builder().token(TOKEN).build()
