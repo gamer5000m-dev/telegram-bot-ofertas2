75cc12a063ef64fd4d6e30c992573ea066108729
gamer5000m-dev <gamer5000m@gmail.com>
2026-05-25 18:04:25 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -71,6 +71,7 @@ async def responder(update, context):
     await context.bot.send_message(
         chat_id=CANAL,
         text=mensagem,
+        parse_mode="HTML",
         disable_web_page_preview=False,
         reply_markup=reply_markup
     )
