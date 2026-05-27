b406b6d03ae2357df7528e853ff2e440ad7b7685
gamer5000m-dev <gamer5000m@gmail.com>
2026-05-27 11:11:16 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from telethon import TelegramClient, events
 import os
 
+# API TELEGRAM
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
+
+# SEU CANAL
+canal_destino = "-1003914285353"
 
 client = TelegramClient(
     "/data/session",
@@ -10,22 +14,33 @@ client = TelegramClient(
     api_hash
 )
 
-canal_destino = "-1003914285353"
-
-@client.on(events.NewMessage)
-async def handler(event):
-
-    print("MENSAGEM RECEBIDA")
-
-    await client.send_message(
-        canal_destino,
-        "TESTE FUNCIONANDO 🔥"
-    )
-
-print("BOT ONLINE")
-
+# LOGIN
 client.start(
-    phone="+5561994348181"
+    phone="+5561994348181",
+    code_callback=lambda: "82195"
 )
+
+print("BOT ONLINE 🔥")
+
+
+# DEBUG PARA PEGAR ID DOS CANAIS
+@client.on(events.NewMessage)
+async def debug(event):
+
+    try:
+
+        print("CHAT ID:", event.chat_id)
+        print("MENSAGEM:", event.raw_text)
+
+        # TESTE
+        await client.send_message(
+            canal_destino,
+            "TESTE FUNCIONANDO 🔥"
+        )
+
+    except Exception as e:
+
+        print(e)
+
 
 client.run_until_disconnected()
