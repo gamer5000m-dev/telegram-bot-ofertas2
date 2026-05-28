7ed38bfbb2d95d37cb41ec955a9088cdbc1e10ae
gamer5000m-dev <gamer5000m@gmail.com>
2026-05-28 13:20:47 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
 from playwright.async_api import async_playwright
-from playwright_stealth import stealth
+from playwright_stealth import Stealth
 import os
 import re
 
@@ -48,7 +48,7 @@ async def gerar_link_afiliado_ml(link_produto):
             page = await browser.new_page()
 
             # STEALTH
-            await stealth(page)
+            await Stealth().apply_stealth_async(page)
 
             await page.goto(
                 "https://www.mercadolivre.com.br/afiliados/linkbuilder",
